67a761a1b3d4d3d4e31ffd0de8378a8ff1530b12
john <jrobertrockwell@gmail.com>
2020-11-26 00:53:32 -0800

    Updated values and added crouching

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,11 +30,14 @@ public class PlayerController : MonoBehaviour
     [Range(0.1f, 100.0f)]
     public float m_SprintSpeed = 8.0f;
 
-    [Range(0.1f, 0.9f)]
-    public float m_CrouchModifier = 0.5f;
+    [Range(0.1f, 5.0f)]
+    public float m_CrouchHeight;
 
     [Range(0.1f, 30.0f)]
     public float m_JumpSpeed = 3.5f;
+
+    [Range(0.01f, 1.0f)]
+    public float m_AirFriction = 0.05f;
 
     [Header("Camera Settings")]
     [Range(0.01f, 3.0f)]
@@ -43,20 +46,25 @@ public class PlayerController : MonoBehaviour
     [Range(0.01f, 3.0f)]
     public float m_CameraPanInSpeed = 0.02f;
 
-    [Range(0.1f, 30.0f)]
-    public float m_WalkingBobSpeed = 12.0f;
+    [Range(0.01f, 3.0f)]
+    public float m_CrouchBobSpeed = 6.0f;
 
     [Range(0.1f, 30.0f)]
-    public float m_SprintingBobSpeed = 25.0f;
+    public float m_WalkingBobSpeed = 9.0f;
+
+    [Range(0.1f, 30.0f)]
+    public float m_SprintingBobSpeed = 18.0f;
 
     [Range(0.01f, 3.0f)]
     public float m_BobbingAmount = 0.03f;
 
-    private const float c_Gravity = -9.81f;
+    private const float c_Gravity = -19.62f; // -9.81
 
     PlayerInput m_PlayerInput;
 
     Vector3 m_PlayerVelocity;
+    float m_CurrentMovementSpeed;
+
     PlayerMovementState m_CurrentMovementState;
     bool m_Grounded;
 
@@ -76,7 +84,10 @@ public class PlayerController : MonoBehaviour
         m_PlayerInput.Player.Crouch.performed += ctx => OnCrouchChanged(ctx);
         m_PlayerInput.Player.Crouch.canceled += ctx => OnCrouchChanged(ctx);
 
+        m_CurrentMovementSpeed = GetExpectedMovementSpeed();
+
         m_DefaultPosY = 0.0f;
+        m_CrouchHeight = 0.0f;
         m_Timer = 0.0f;
     }
 
@@ -97,6 +108,7 @@ public class PlayerController : MonoBehaviour
     {
         m_DefaultCameraFov = m_Camera.fieldOfView;
         m_DefaultPosY = m_Camera.transform.localPosition.y;
+        m_CrouchHeight = m_DefaultPosY / 2;
     }
 
     // Fire every frame
@@ -107,12 +119,10 @@ public class PlayerController : MonoBehaviour
         UpdateMovement(m_PlayerInput.Player.Move.ReadValue<Vector2>());
         UpdateFieldOfView();
         UpdateViewBobbing();
+        UpdateHeight();
 
         if ( m_Grounded && m_PlayerVelocity.y < 0 )
            m_PlayerVelocity.y = -2.0f;
-
-        if ( !m_Grounded && m_PlayerVelocity.y < 0.0f && m_CurrentMovementState == PlayerMovementState.Sprint )
-            m_CurrentMovementState = PlayerMovementState.Walk;
 
         m_PlayerVelocity.y += c_Gravity * Time.deltaTime;
 
@@ -122,9 +132,10 @@ public class PlayerController : MonoBehaviour
     // Handles movement updates from the input system
     private void UpdateMovement(Vector2 p_Data)
     {
-        Vector3 m_Movement = (transform.right * p_Data.x + transform.forward * p_Data.y) * GetMovementSpeed();
+        Vector3 m_Movement = (transform.right * p_Data.x + transform.forward * p_Data.y) * GetCurrentMovementSpeed();
         m_PlayerVelocity.x = m_Movement.x;
         m_PlayerVelocity.z = m_Movement.z;
+
 
         // Player is not holding W, if they are sprinting we need to set them to walking
         if ( p_Data.y != 1 && m_CurrentMovementState == PlayerMovementState.Sprint )
@@ -145,17 +156,28 @@ public class PlayerController : MonoBehaviour
     // Handles updating view bobbing for the player depending on their movement state
     private void UpdateViewBobbing()
     {
+        float m_Height = (m_CurrentMovementState == PlayerMovementState.Crouch) ? m_CrouchHeight : m_DefaultPosY;
+
         if (Mathf.Abs(m_PlayerVelocity.x) > 0.1f || Mathf.Abs(m_PlayerVelocity.z) > 0.1f)
         {
             m_Timer += Time.deltaTime * GetViewBobbingSpeed();
-            m_Camera.transform.localPosition = new Vector3(m_Camera.transform.localPosition.x, m_DefaultPosY + Mathf.Sin(m_Timer) * m_BobbingAmount, m_Camera.transform.localPosition.z);
+            m_Camera.transform.localPosition = new Vector3(m_Camera.transform.localPosition.x, m_Height + Mathf.Sin(m_Timer) * m_BobbingAmount, m_Camera.transform.localPosition.z);
         }
 
         else
         {
             m_Timer = 0.0f;
-            m_Camera.transform.localPosition = new Vector3(m_Camera.transform.localPosition.x, Mathf.Lerp(m_Camera.transform.localPosition.y, m_DefaultPosY, Time.deltaTime * m_WalkingBobSpeed), m_Camera.transform.localPosition.z);
+            m_Camera.transform.localPosition = new Vector3(m_Camera.transform.localPosition.x, Mathf.Lerp(m_Camera.transform.localPosition.y, m_Height, Time.deltaTime * m_WalkingBobSpeed), m_Camera.transform.localPosition.z);
         }
+    }
+
+    // Handles updating the player camera height when crouching
+    private void UpdateHeight()
+    {
+        if ( m_CurrentMovementState == PlayerMovementState.Crouch )
+            m_Camera.transform.localPosition = Vector3.Lerp(m_Camera.transform.localPosition, new Vector3(m_Camera.transform.localPosition.x, m_CrouchHeight, m_Camera.transform.localPosition.z), 0.02f);
+        else
+            m_Camera.transform.localPosition = Vector3.Lerp(m_Camera.transform.localPosition, new Vector3(m_Camera.transform.localPosition.x, m_DefaultPosY, m_Camera.transform.localPosition.z), 0.02f);
     }
 
     // Handles jump requests from the input system
@@ -202,15 +224,22 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Returns the movement speed for the player based on their current movement state
-    private float GetMovementSpeed()
+    // Returns the interpolated movement speed for the player currently
+    private float GetCurrentMovementSpeed()
+    {
+        float m_ExpectedMovementSpeed = GetExpectedMovementSpeed();
+        return Mathf.Lerp(m_CurrentMovementSpeed, m_ExpectedMovementSpeed, m_AirFriction);
+    }
+
+    // Returns the target speed for a player depending on their current movement state
+    private float GetExpectedMovementSpeed()
     {
         switch ( m_CurrentMovementState )
         {
             case PlayerMovementState.Walk:
                 return m_WalkSpeed;
             case PlayerMovementState.Crouch:
-                return (m_WalkSpeed * m_CrouchModifier);
+                return (m_WalkSpeed / 2);
             case PlayerMovementState.Sprint:
                 return m_SprintSpeed;
             default:
@@ -225,6 +254,8 @@ public class PlayerController : MonoBehaviour
         {
             case PlayerMovementState.Sprint:
                 return m_SprintingBobSpeed;
+            case PlayerMovementState.Crouch:
+                return m_CrouchBobSpeed;
             default:
                 return m_WalkingBobSpeed;
         }
